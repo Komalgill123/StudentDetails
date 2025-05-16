7df1ea02012b2d88886fec83a260c00b007e827d
komalgill123 <komalp44@sheridancollege.ca>
2025-05-16 18:27:28 -0400

    student version 1

---
+++ src/studentlist/Student.java
@@ -0,0 +1,46 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package studentlist;
+
+/**
+ *
+ * @author User
+ */
+public class Student {
+    //model student details
+    private String name; //principle encapsulation
+    private int sid;
+    
+    //getter and setter
+
+    /**
+     * @return the name
+     */
+    public String getName() {
+        return name;
+    }
+
+    /**
+     * @param name the name to set
+     */
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    /**
+     * @return the sid
+     */
+    public int getSid() {
+        return sid;
+    }
+
+    /**
+     * @param sid the sid to set
+     */
+    public void setSid(int sid) {
+        this.sid = sid;
+    }
+    
+}
