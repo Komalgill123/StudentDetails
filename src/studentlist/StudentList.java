8934d304d9e77ea99293b3ad5019f0d628803fe7
komalgill123 <komalp44@sheridancollege.ca>
2025-05-26 16:58:50 -0400

    student version 5

--- src/studentlist/StudentList.java
+++ src/studentlist/StudentList.java
@@ -32,7 +32,7 @@ public class StudentList {
         Student[] list = new Student[3]; //object of student in array
         list[0]=s1;
         list[1]=s2;
-        list[2]=s3;
+        
         for(int i=0;i<list.length;i++){
         System.out.println(list[i].getName()+" "+list[i].getSid());
     }
